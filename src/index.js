000ccb7063bfb23918f35aa9ca8dbbf27e5e7171
christophersesugh <christohybrid185@gmail.com>
2023-10-24 12:32:38 +0100

    index.js

--- src/index.js
+++ src/index.js
@@ -95,5 +95,5 @@ app.use(notFound);
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
-  console.log(`Server running on port: ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
